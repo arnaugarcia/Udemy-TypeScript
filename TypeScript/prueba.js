e83702e6268ee1f7af706b091f7489c00bd57076
Arnau García <arnau.garcia.gallego@gmail.com>
2017-01-30 22:50:30 +0100

    V8 - Tipos de variables y diferencias entre (let y var)

--- TypeScript/prueba.js
+++ TypeScript/prueba.js
@@ -4,7 +4,22 @@
 function holaMundo(nombre) {
     return "Hola mundo, soy " + nombre;
 }
-var nombre = "Arnau García";
+//var nombre = "Arnau García";
 var resultado = holaMundo(nombre);
 var etiqueta = document.getElementById("container");
 etiqueta.innerHTML = holaMundo(nombre);
+//Variables y tipos
+var nombre = "Arnau";
+var edad = 19;
+var programador = true;
+var langs = ["PHP", "Javascript", "CSS"];
+etiqueta.innerHTML = nombre + " - " + edad;
+// let y var
+var a = 7;
+var b = 1;
+if (a === 7) {
+    var a_1 = 4;
+    var b = 12;
+    console.log("Dentro del IF " + a_1 + " - " + b);
+}
+console.log("Fuera del IF " + a + " - " + b);
--- TypeScript/prueba.ts
+++ TypeScript/prueba.ts
@@ -5,10 +5,36 @@ function holaMundo(nombre){
     return "Hola mundo, soy " + nombre;
 }
 
-let nombre = "Arnau García";
+//var nombre = "Arnau García";
 
 var resultado = holaMundo(nombre);
 
 var etiqueta = <HTMLElement>document.getElementById("container");
 
 etiqueta.innerHTML = holaMundo(nombre);
+
+//Variables y tipos
+var nombre:string = "Arnau";
+
+var edad:number = 19;
+
+var programador:boolean = true;
+
+var langs: Array<String> = ["PHP","Javascript","CSS"];
+
+etiqueta.innerHTML = nombre + " - " + edad;
+
+
+// let y var
+
+var a = 7;
+var b = 1;
+
+if (a === 7){
+
+    let a = 4;
+    var b = 12;
+
+    console.log("Dentro del IF " + a + " - " + b);
+}
+console.log("Fuera del IF " + a + " - " + b);
